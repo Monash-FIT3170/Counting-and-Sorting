8204d4622637d25ec303518786bb977a6717de38
ismailH123 <Ihassan04@outlook.com>
2024-05-15 23:49:25 +1000

    Added a class called listOfShoppingList. This class is a singleton and will act as a database of shopping lists to demonstrate the creation of a shopping list until we set up our database.

---
+++ inventory-management-system/src/main/java/com/business/application/domain/ListOfShoppingList.java
@@ -0,0 +1,34 @@
+package com.business.application.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfShoppingList {
+    private static ListOfShoppingList instance;
+    private List<ShoppingList> shoppingLists;
+
+    private ListOfShoppingList() {
+        // Private constructor to prevent instantiation from outside
+        shoppingLists = new ArrayList<>();
+    }
+
+    public static synchronized ListOfShoppingList getInstance() {
+        if (instance == null) {
+            instance = new ListOfShoppingList();
+        }
+        return instance;
+    }
+
+    public List<ShoppingList> getShoppingLists() {
+        return shoppingLists;
+    }
+
+    public void addShoppingList(ShoppingList shoppingList) {
+        shoppingLists.add(shoppingList);
+    }
+
+    public void removeShoppingList(ShoppingList shoppingList) {
+        shoppingLists.remove(shoppingList);
+    }
+
+}
