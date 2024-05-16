a1e9d0cf2d711cfe92c67d8eb2d8a0111ab86243
Behnam Mozafari <bmoz0002@student.monash.edu>
2024-05-17 01:03:56 +1000

    Added store search bar, updated chart lable and year select

--- inventory-management-system/src/main/java/com/business/application/views/admindashboard/AdminDashboardView.java
+++ inventory-management-system/src/main/java/com/business/application/views/admindashboard/AdminDashboardView.java
@@ -11,6 +11,7 @@ import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
@@ -19,6 +20,8 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -38,12 +41,37 @@ public class AdminDashboardView extends Main {
     public AdminDashboardView() {
         addClassName("admin-dashboard-view");
 
+        HorizontalLayout storeInfoLayout = createStoreInfoLayout();
+        add(storeInfoLayout);
+
         Board board = new Board();
         board.addRow(createHighlight("Current users", "745", 33.7), createHighlight("View events", "54.6k", -112.45),
                 createHighlight("Conversion rate", "18%", 3.9), createHighlight("Custom metric", "-123.45", 0.0));
-        board.addRow(createViewEvents());
-        board.addRow(createServiceHealth(), createResponseTimes());
+        board.addRow(createViewSalesQty());
         add(board);
+    }
+    
+    private HorizontalLayout createStoreInfoLayout()  {
+        H1 storeName = new H1("CLAYTON");
+        storeName.addClassNames(FontWeight.NORMAL, Margin.NONE, TextColor.SECONDARY, FontSize.MEDIUM);
+
+        // Search bar
+        TextField searchBar = new TextField();
+        //<theme-editor-local-classname>
+        searchBar.addClassName("admin-dashboard-view-store-search");
+        searchBar.setPlaceholder("Select Store");
+        searchBar.setSuffixComponent(new Icon(VaadinIcon.SEARCH));
+        searchBar.setWidth("300px");
+
+        // Layout for store info and search bar
+        HorizontalLayout layout = createHeader("CLAYTON", "");
+        layout.add(searchBar);
+        layout.setWidthFull();
+        layout.setHeight("50px");
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        layout.addClassName(Padding.LARGE);
+        return layout;
     }
 
     private Component createHighlight(String title, String value, Double percentage) {
@@ -80,16 +108,19 @@ public class AdminDashboardView extends Main {
         return layout;
     }
 
-    private Component createViewEvents() {
+    private Component createViewSalesQty() {
         // Header
-        Select year = new Select();
-        year.setItems("2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021");
-        year.setValue("2021");
-        year.setWidth("100px");
+        IntegerField year = new IntegerField();
+        //<theme-editor-local-classname>
+        year.addClassName("admin-dashboard-view-year-1");
+        year.setValue(2024);
+        year.setStepButtonsVisible(true);
+        year.setMin(1970);
+        year.setMax(2024);
 
-        HorizontalLayout header = createHeader("View events", "City/month");
+        HorizontalLayout header = createHeader("VIEW SALES QTY", "");
         header.add(year);
-
+        header.setAlignItems(FlexComponent.Alignment.CENTER);
         // Chart
         Chart chart = new Chart(ChartType.AREASPLINE);
         Configuration conf = chart.getConfiguration();
@@ -120,70 +151,10 @@ public class AdminDashboardView extends Main {
         return viewEvents;
     }
 
-    private Component createServiceHealth() {
-        // Header
-        HorizontalLayout header = createHeader("Service health", "Input / output");
-
-        // Grid
-        Grid<ServiceHealth> grid = new Grid();
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-        grid.setAllRowsVisible(true);
-
-        grid.addColumn(new ComponentRenderer<>(serviceHealth -> {
-            Span status = new Span();
-            String statusText = getStatusDisplayName(serviceHealth);
-            status.getElement().setAttribute("aria-label", "Status: " + statusText);
-            status.getElement().setAttribute("title", "Status: " + statusText);
-            status.getElement().getThemeList().add(getStatusTheme(serviceHealth));
-            return status;
-        })).setHeader("").setFlexGrow(0).setAutoWidth(true);
-        grid.addColumn(ServiceHealth::getCity).setHeader("City").setFlexGrow(1);
-        grid.addColumn(ServiceHealth::getInput).setHeader("Input").setAutoWidth(true).setTextAlign(ColumnTextAlign.END);
-        grid.addColumn(ServiceHealth::getOutput).setHeader("Output").setAutoWidth(true)
-                .setTextAlign(ColumnTextAlign.END);
-
-        grid.setItems(new ServiceHealth(Status.EXCELLENT, "Münster", 324, 1540),
-                new ServiceHealth(Status.OK, "Cluj-Napoca", 311, 1320),
-                new ServiceHealth(Status.FAILING, "Ciudad Victoria", 300, 1219));
-
-        // Add it all together
-        VerticalLayout serviceHealth = new VerticalLayout(header, grid);
-        serviceHealth.addClassName(Padding.LARGE);
-        serviceHealth.setPadding(false);
-        serviceHealth.setSpacing(false);
-        serviceHealth.getElement().getThemeList().add("spacing-l");
-        return serviceHealth;
-    }
-
-    private Component createResponseTimes() {
-        HorizontalLayout header = createHeader("Response times", "Average across all systems");
-
-        // Chart
-        Chart chart = new Chart(ChartType.PIE);
-        Configuration conf = chart.getConfiguration();
-        conf.getChart().setStyledMode(true);
-        chart.setThemeName("gradient");
-
-        DataSeries series = new DataSeries();
-        series.add(new DataSeriesItem("System 1", 12.5));
-        series.add(new DataSeriesItem("System 2", 12.5));
-        series.add(new DataSeriesItem("System 3", 12.5));
-        series.add(new DataSeriesItem("System 4", 12.5));
-        series.add(new DataSeriesItem("System 5", 12.5));
-        series.add(new DataSeriesItem("System 6", 12.5));
-        conf.addSeries(series);
-
-        // Add it all together
-        VerticalLayout serviceHealth = new VerticalLayout(header, chart);
-        serviceHealth.addClassName(Padding.LARGE);
-        serviceHealth.setPadding(false);
-        serviceHealth.setSpacing(false);
-        serviceHealth.getElement().getThemeList().add("spacing-l");
-        return serviceHealth;
-    }
-
     private HorizontalLayout createHeader(String title, String subtitle) {
         H2 h2 = new H2(title);
+        //<theme-editor-local-classname>
+        h2.addClassName("admin-dashboard-view-h2-1");
         h2.addClassNames(FontSize.XLARGE, Margin.NONE);
 
         Span span = new Span(subtitle);
@@ -198,30 +169,6 @@ public class AdminDashboardView extends Main {
         header.setSpacing(false);
         header.setWidthFull();
         return header;
-    }
-
-    private String getStatusDisplayName(ServiceHealth serviceHealth) {
-        Status status = serviceHealth.getStatus();
-        if (status == Status.OK) {
-            return "Ok";
-        } else if (status == Status.FAILING) {
-            return "Failing";
-        } else if (status == Status.EXCELLENT) {
-            return "Excellent";
-        } else {
-            return status.toString();
-        }
-    }
-
-    private String getStatusTheme(ServiceHealth serviceHealth) {
-        Status status = serviceHealth.getStatus();
-        String theme = "badge primary small";
-        if (status == Status.EXCELLENT) {
-            theme += " success";
-        } else if (status == Status.FAILING) {
-            theme += " error";
-        }
-        return theme;
     }
 
 }
