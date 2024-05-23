ed5a2d2bbaaf3ee348cd272b2c3b11a256934e34
g-vohra <garv.vohra21@gmail.com>
2024-05-24 05:32:33 +1000

    Updated Product Data method

--- inventory-management-system/src/main/java/com/business/application/views/adminstock/AdminRestockingRequestView.java
+++ inventory-management-system/src/main/java/com/business/application/views/adminstock/AdminRestockingRequestView.java
@@ -117,9 +117,9 @@ public class AdminRestockingRequestView extends Composite<VerticalLayout> {
         sampleProducts.add(product1);
 
         Product product2 = new Product();
-        product2.setProductId(1902L);
-        product2.setName("Brandy");
-        product2.setCategory("Alcahol");
+        product2.setProductId(19802L);
+        product2.setName("Red Wine");
+        product2.setCategory("Wine");
         product2.setCurrentQuantity(100);
         product2.setRequestedQuantity(50);
         sampleProducts.add(product2);
