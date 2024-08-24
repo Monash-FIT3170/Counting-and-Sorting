ea6572d9b17943aa0a9458a8ad8ab3adf9c5a2f6
arud0001 <arud0001@student.monash.edu>
2024-08-24 18:14:58 +1000

    Graphs Based on Stores

--- inventory-management-system/src/main/java/com/business/application/views/finance/AdminFinanceView.java
+++ inventory-management-system/src/main/java/com/business/application/views/finance/AdminFinanceView.java
@@ -32,6 +32,8 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
 
 import jakarta.annotation.security.RolesAllowed;
+import java.util.Arrays;
+import java.util.List;
 
 @PageTitle("Admin Finance")
 @Route(value = "admin-finance-view", layout = MainLayout.class)
@@ -57,7 +59,7 @@ public class AdminFinanceView extends Div {
         storeSelect.setLabel("Select Store");
         storeSelect.setPlaceholder("Select Store");
         storeSelect.setItemLabelGenerator(Store::getName);
-        storeSelect.addValueChangeListener(event -> updateHighlights(event.getValue()));
+        storeSelect.addValueChangeListener(event -> updateHighlightsAndGraph(event.getValue()));
 
         HorizontalLayout storeSelectorLayout = new HorizontalLayout();
         storeSelectorLayout.setWidthFull();
@@ -70,14 +72,13 @@ public class AdminFinanceView extends Div {
         selectionComboBox.setItems("Graph", "Table");
 
         contentContainer = new VerticalLayout();
-        contentContainer.add(createFinancialGraph());
 
         selectionComboBox.addValueChangeListener(event -> {
             contentContainer.removeAll();
             if ("Graph".equals(event.getValue())) {
-                contentContainer.add(createFinancialGraph());
+                contentContainer.add(createFinancialGraph(storeSelect.getValue()));
             } else if ("Table".equals(event.getValue())) {
-                contentContainer.add(createList());
+                contentContainer.add(createList(storeSelect.getValue()));
             }
         });
 
@@ -88,20 +89,38 @@ public class AdminFinanceView extends Div {
     }
 
     private void initializeStores() {
-        Store store1 = new Store("Store 1", 10000, 50000, 40000, 10000);
-        Store store2 = new Store("Store 2", 20000, 60000, 40000, 20000);
-        Store store3 = new Store("Store 3", 30000, 70000, 40000, 30000);
+        Store store1 = new Store("Store 1",
+                10000, 50000, 40000, 10000,
+                Arrays.asList(109, 141, 91, 126),
+                Arrays.asList(138, 190, 109, 150),
+                Arrays.asList(65, 61, 66, 71));
+
+        Store store2 = new Store("Store 2",
+                20000, 60000, 40000, 20000,
+                Arrays.asList(85, 130, 70, 100),
+                Arrays.asList(110, 170, 90, 140),
+                Arrays.asList(55, 50, 55, 60));
+
+        Store store3 = new Store("Store 3",
+                30000, 70000, 40000, 30000,
+                Arrays.asList(120, 150, 100, 130),
+                Arrays.asList(150, 200, 120, 160),
+                Arrays.asList(70, 65, 70, 75));
 
         storeSelect.setItems(store1, store2, store3);
         storeSelect.setValue(store1); // Set Store 1 as the default selected store
     }
 
-    private void updateHighlights(Store store) {
+    private void updateHighlightsAndGraph(Store store) {
         if (store == null) return;
 
         highlightsLayout.removeAll();
         highlightsLayout.add(createHighlight("Cash on Hand", "$" + store.getCashOnHand(), 0.0),
                              createHighlight("Projected Profit", "$" + store.getProfit(), calculatePercentage(store.getProfit(), store.getRevenue())));
+
+        // Update the graph when the store is changed
+        contentContainer.removeAll();
+        contentContainer.add(createFinancialGraph(store));
     }
 
     private double calculatePercentage(int profit, int revenue) {
@@ -144,9 +163,8 @@ public class AdminFinanceView extends Div {
         return layout;
     }
 
-    // Reuse methods from UserFinanceView
-
-    private Component createFinancialGraph() {
+    // Method to create a dynamic financial graph based on store data
+    private Component createFinancialGraph(Store store) {
         // Header
         IntegerField year = new IntegerField();
         year.addClassName("user-financial-graph");
@@ -154,35 +172,35 @@ public class AdminFinanceView extends Div {
         year.setStepButtonsVisible(true);
         year.setMin(2020);
         year.setMax(2024);
-
-        HorizontalLayout header = createHeader("Yearly Financial Analysis", "");
+    
+        HorizontalLayout header = createHeader("Yearly Financial Analysis", store.getName());
         header.add(year);
         header.setAlignItems(FlexComponent.Alignment.CENTER);
-
+    
         // Chart
         Chart chart = new Chart(ChartType.SPLINE);
         Configuration conf = chart.getConfiguration();
         conf.getChart().setStyledMode(true);
         chart.setWidth("97%");
-
+    
         XAxis xAxis = new XAxis();
         xAxis.setCategories("Q1", "Q2", "Q3", "Q4");
         conf.addxAxis(xAxis);
-
+    
         YAxis yAxis = new YAxis();
         yAxis.setTitle("$k");
-        yAxis.setCategories("20", "40", "60", "80", "100", "120", "140", "160", "180", "200");
         conf.addyAxis(yAxis);
-
+    
         PlotOptionsSpline plotOptions = new PlotOptionsSpline();
         plotOptions.setPointPlacement(PointPlacement.ON);
         plotOptions.setMarker(new Marker(false));
         conf.addPlotOptions(plotOptions);
-
-        conf.addSeries(new ListSeries("Profit", 109, 141, 91, 126));
-        conf.addSeries(new ListSeries("Income", 138, 190, 109, 150));
-        conf.addSeries(new ListSeries("Expenses", 65, 61, 66, 71));
-
+    
+        // Convert List<Integer> to Integer[] and add to series
+        conf.addSeries(new ListSeries("Profit", store.getQuarterlyProfit().toArray(new Integer[0])));
+        conf.addSeries(new ListSeries("Income", store.getQuarterlyIncome().toArray(new Integer[0])));
+        conf.addSeries(new ListSeries("Expenses", store.getQuarterlyExpenses().toArray(new Integer[0])));
+    
         // Add it all together
         VerticalLayout viewEvents = new VerticalLayout(header, chart);
         viewEvents.addClassName("Graph");
@@ -194,9 +212,9 @@ public class AdminFinanceView extends Div {
         return viewEvents;
     }
 
-    private Grid<String> createList() {
+    private Grid<String> createList(Store store) {
         Grid<String> grid = new Grid<>();
-        grid.setItems("Item 1", "Item 2", "Item 3");
+        grid.setItems("Item 1", "Item 2", "Item 3"); // Example items, you can customize this to show store-specific data
         grid.addColumn(item -> item).setHeader("Items");
 
         return grid;
@@ -221,19 +239,27 @@ public class AdminFinanceView extends Div {
         return header;
     }
 
-    private class Store {
+    // Store class with financial data and methods
+    public static class Store {
         private final String name;
         private final Integer profit;
         private final Integer revenue;
         private final Integer expenses;
         private final Integer cashOnHand;
+        private final List<Integer> quarterlyProfit;
+        private final List<Integer> quarterlyIncome;
+        private final List<Integer> quarterlyExpenses;
 
-        public Store(String name, Integer profit, Integer revenue, Integer expenses, Integer cashOnHand) {
+        public Store(String name, Integer profit, Integer revenue, Integer expenses, Integer cashOnHand,
+                     List<Integer> quarterlyProfit, List<Integer> quarterlyIncome, List<Integer> quarterlyExpenses) {
             this.name = name;
             this.profit = profit;
             this.revenue = revenue;
             this.expenses = expenses;
             this.cashOnHand = cashOnHand;
+            this.quarterlyProfit = quarterlyProfit;
+            this.quarterlyIncome = quarterlyIncome;
+            this.quarterlyExpenses = quarterlyExpenses;
         }
 
         public String getName() {
@@ -254,6 +280,18 @@ public class AdminFinanceView extends Div {
 
         public Integer getCashOnHand() {
             return cashOnHand;
+        }
+
+        public List<Integer> getQuarterlyProfit() {
+            return quarterlyProfit;
+        }
+
+        public List<Integer> getQuarterlyIncome() {
+            return quarterlyIncome;
+        }
+
+        public List<Integer> getQuarterlyExpenses() {
+            return quarterlyExpenses;
         }
     }
 }
