148d8444250be8d9f8fab25f04b791d9aaf27ff4
Jesse <jgor0013@student.monash.edu>
2024-08-24 16:22:23 +1000

    Generate sample data for inventory management system

--- inventory-management-system/frontend/generated/vaadin.ts
+++ inventory-management-system/frontend/generated/vaadin.ts
@@ -2,8 +2,6 @@ import './vaadin-featureflags.js';
 
 import './index';
 
-import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
 import './theme-liquor-store.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
