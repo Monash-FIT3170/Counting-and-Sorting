3b1ea20d99e45a6889002e915c4b1dfb601b34ef
Behnam Mozafari <bmoz0002@student.monash.edu>
2024-08-25 17:18:40 +1000

    Feature, Suppliers View, updated suppliers view to match figma changed SuppliersView.java, suppliers-view.css, theme-editor.css

--- inventory-management-system/src/main/java/com/business/application/views/suppliers/SuppliersView.java
+++ inventory-management-system/src/main/java/com/business/application/views/suppliers/SuppliersView.java
@@ -13,6 +13,7 @@ import com.vaadin.flow.component.grid.HeaderRow;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -20,6 +21,7 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
@@ -28,6 +30,12 @@ import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoIcon;
+import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
+import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
+
 import jakarta.annotation.security.RolesAllowed;
 import java.text.NumberFormat;
 import java.time.LocalDate;
@@ -40,7 +48,6 @@ import org.apache.commons.lang3.StringUtils;
 import com.vaadin.flow.component.notification.Notification;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 @PageTitle("Suppliers")
 @Route(value = "data-grid2", layout = MainLayout.class)
@@ -60,12 +67,24 @@ public class SuppliersView extends Div {
 
     @Autowired
     public SuppliersView(WebScrapedProductService webscrapedProductService) {
-        this.webscrapedProductService =  webscrapedProductService;
+        this.webscrapedProductService = webscrapedProductService;
         addClassName("suppliers-view");
-        setSizeFull();
+        // Create the toolbar
+        HorizontalLayout toolbar = createToolbar();
+
+        // Create the grid
         createGrid();
         createSuppliers();
-        add(createToolbar(), grid);
+
+        // Layout
+        VerticalLayout mainLayout = new VerticalLayout(toolbar, grid);
+        mainLayout.setSizeFull();
+        mainLayout.setPadding(false);
+        mainLayout.setSpacing(false);
+        grid.getElement().getStyle().set("margin-top", "16px");
+
+        // Add to the view
+        add(mainLayout);
     }
 
     private void createSuppliers() {
@@ -104,22 +123,22 @@ public class SuppliersView extends Div {
 
     private void createCategoryColumn() {
         CategoryColumn = grid.addColumn(new ComponentRenderer<>(supplier -> {
-                    Span categorySpan = new Span();
-                    String category = supplier.getCategory();
-                    categorySpan.setText(category);
-                    if ("Wine".equals(category)) {
-                        categorySpan.getElement().setAttribute("class", "badge-wine");
-                    } else if ("Beer".equals(category)) {
-                        categorySpan.getElement().setAttribute("class", "badge-beer");
-                    } else if ("Spirits".equals(category)) {
-                        categorySpan.getElement().setAttribute("class", "badge-spirit");
-                    } else if ("Cider".equals(category)) {
-                        categorySpan.getElement().setAttribute("class", "badge-cider");
-                    } else {
-                        categorySpan.getElement().setAttribute("class", "badge-misc");
-                    }
-                    return categorySpan;
-                }))
+            Span categorySpan = new Span();
+            String category = supplier.getCategory();
+            categorySpan.setText(category);
+            if ("Wine".equals(category)) {
+                categorySpan.getElement().setAttribute("class", "badge-wine");
+            } else if ("Beer".equals(category)) {
+                categorySpan.getElement().setAttribute("class", "badge-beer");
+            } else if ("Spirits".equals(category)) {
+                categorySpan.getElement().setAttribute("class", "badge-spirit");
+            } else if ("Cider".equals(category)) {
+                categorySpan.getElement().setAttribute("class", "badge-cider");
+            } else {
+                categorySpan.getElement().setAttribute("class", "badge-misc");
+            }
+            return categorySpan;
+        }))
                 .setHeader("Category");
     }
 
@@ -138,43 +157,58 @@ public class SuppliersView extends Div {
     }
 
     private HorizontalLayout createToolbar() {
-        TextField searchField = new TextField();
-        searchField.setPlaceholder("Search Items");
-        searchField.setPrefixComponent(VaadinIcon.SEARCH.create());
-        searchField.setWidth("300px");
+        // Create the search bar
+        TextField searchBar = new TextField();
+        searchBar.addClassName("supplier-view-item-search");
+        searchBar.setPlaceholder("Search Items");
+        searchBar.setSuffixComponent(LumoIcon.SEARCH.create());
+        searchBar.setWidth("300px");
 
-        Button refreshButton = new Button("Refresh", VaadinIcon.REFRESH.create());
-        refreshButton.addClickListener(click -> refreshGridData());
-
-        HorizontalLayout toolbar = new HorizontalLayout(searchField, refreshButton);
-        toolbar.addClassName("toolbar");
+        // Create the layout for the label and search field
+        HorizontalLayout toolbar = createHeader("SUPPLIERS", "");
+        toolbar.add(searchBar);
+        toolbar.setWidthFull();
+        toolbar.setHeight("50px");
+        toolbar.setAlignItems(FlexComponent.Alignment.CENTER);
+        toolbar.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        toolbar.addClassName(Padding.LARGE);
+        toolbar.addClassName("search-top-section");
         return toolbar;
     }
 
-    private void refreshGridData() {
-        // replace this line with a service call the webscraper endpoint 
-        suppliers = webscrapedProductService.getAllWebscrapedProducts();
-        gridListDataView = grid.setItems(suppliers);
-        grid.getDataProvider().refreshAll();
-        grid.deselectAll();
-        // Wait 5 seconds
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Notification.show("Data refreshed");
+    private HorizontalLayout createHeader(String title, String subtitle) {
+        H2 h2 = new H2(title);
+        h2.addClassName("admin-dashboard-view-h2-1");
+        h2.addClassNames(FontSize.XLARGE, Margin.NONE);
+
+        Span span = new Span(subtitle);
+        span.addClassNames(TextColor.SECONDARY, FontSize.XSMALL);
+
+        VerticalLayout column = new VerticalLayout(h2, span);
+        column.setPadding(false);
+        column.setSpacing(false);
+
+        HorizontalLayout header = new HorizontalLayout(column);
+        header.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        header.setSpacing(false);
+        header.setWidthFull();
+        return header;
     }
 
     private void addFiltersToGrid() {
         HeaderRow filterRow = grid.appendHeaderRow();
 
         TextField nameFilter = new TextField();
-        nameFilter.setPlaceholder("Filter");
+        nameFilter.setPlaceholder("Search");
         nameFilter.setClearButtonVisible(true);
         nameFilter.setWidth("100%");
         nameFilter.setValueChangeMode(ValueChangeMode.EAGER);
-        nameFilter.addValueChangeListener(event -> gridListDataView.addFilter(supplier -> StringUtils.containsIgnoreCase(supplier.getName(), nameFilter.getValue())));
+        nameFilter.addValueChangeListener(event -> {
+            // Clear existing filters before adding the new one
+            gridListDataView.removeFilters();
+            gridListDataView
+                    .addFilter(supplier -> StringUtils.containsIgnoreCase(supplier.getName(), nameFilter.getValue()));
+        });
         filterRow.getCell(ItemNameColumn).setComponent(nameFilter);
 
         ComboBox<String> categoryFilter = new ComboBox<>();
@@ -182,29 +216,44 @@ public class SuppliersView extends Div {
         categoryFilter.setPlaceholder("Filter");
         categoryFilter.setClearButtonVisible(true);
         categoryFilter.setWidth("100%");
-        categoryFilter.addValueChangeListener(event -> gridListDataView.addFilter(supplier -> {
-            String filterValue = categoryFilter.getValue();
-            if (filterValue != null) {
-                return filterValue.equalsIgnoreCase(supplier.getCategory());
-            }
-            return true;
-        }));
+        categoryFilter.addValueChangeListener(event -> {
+            // Clear existing filters before adding the new one
+            gridListDataView.removeFilters();
+            gridListDataView.addFilter(supplier -> {
+                String filterValue = categoryFilter.getValue();
+                if (filterValue != null) {
+                    return filterValue.equalsIgnoreCase(supplier.getCategory());
+                }
+                return true;
+            });
+        });
         filterRow.getCell(CategoryColumn).setComponent(categoryFilter);
 
         TextField salePriceFilter = new TextField();
-        salePriceFilter.setPlaceholder("Filter");
+        salePriceFilter.setPlaceholder("Search");
         salePriceFilter.setClearButtonVisible(true);
         salePriceFilter.setWidth("100%");
         salePriceFilter.setValueChangeMode(ValueChangeMode.EAGER);
-        salePriceFilter.addValueChangeListener(event -> gridListDataView.addFilter(supplier -> StringUtils.containsIgnoreCase(Double.toString(supplier.getPrice()), salePriceFilter.getValue())));
+        salePriceFilter.addValueChangeListener(event -> {
+            // Clear existing filters before adding the new one
+            gridListDataView.removeFilters();
+            gridListDataView.addFilter(supplier -> StringUtils
+                    .containsIgnoreCase(Double.toString(supplier.getPrice()), salePriceFilter.getValue()));
+        });
         filterRow.getCell(SalePriceColumn).setComponent(salePriceFilter);
 
         TextField supplierFilter = new TextField();
-        supplierFilter.setPlaceholder("Filter");
+        supplierFilter.setPlaceholder("Search");
         supplierFilter.setClearButtonVisible(true);
         supplierFilter.setWidth("100%");
         supplierFilter.setValueChangeMode(ValueChangeMode.EAGER);
-        supplierFilter.addValueChangeListener(event -> gridListDataView.addFilter(supplier -> StringUtils.containsIgnoreCase(supplier.getSupplier(), supplierFilter.getValue())));
+        supplierFilter.addValueChangeListener(event -> {
+            // Clear existing filters before adding the new one
+            gridListDataView.removeFilters();
+            gridListDataView.addFilter(
+                    supplier -> StringUtils.containsIgnoreCase(supplier.getSupplier(), supplierFilter.getValue()));
+        });
         filterRow.getCell(SupplierColumn).setComponent(supplierFilter);
     }
-}
+
+};
